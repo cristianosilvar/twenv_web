b9b3f1662550e2314cd80095bd3180eb72ff01ea
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-28 10:30:45 -0300

    First Commit

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,6 @@ import { ColorModeScript } from "@chakra-ui/react"
 import * as React from "react"
 import * as ReactDOM from "react-dom/client"
 import { App } from "./App"
-import reportWebVitals from "./reportWebVitals"
 import * as serviceWorker from "./serviceWorker"
 
 
@@ -21,9 +20,4 @@ root.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorker.unregister()
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
 
