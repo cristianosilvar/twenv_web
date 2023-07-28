e3778e2373859e92c8e4ea4b1d671b9f3e8591ae
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-28 20:49:08 -0300

    Adicionando o componente Header

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,10 @@ import {
   ChakraProvider,
   theme
 } from "@chakra-ui/react"
+import { Header } from "./components/Header/Header"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
+    <Header/>
   </ChakraProvider>
 )
