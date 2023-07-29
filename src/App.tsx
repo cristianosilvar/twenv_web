eb2fcaca9fa0efb390b4b9cf930de7d9d333b53a
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-28 21:09:31 -0300

    Adicionando pages 'Spending' e 'Earnings'

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,23 @@ import {
   ChakraProvider,
   theme
 } from "@chakra-ui/react"
+
 import { Header } from "./components/Header/Header"
+
+import Spending from "./pages/Spending/Spending"
+import Earnings from "./pages/Earnings/Earnings"
+
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Header/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/spending" element={<Spending/>} />
+        <Route path="/earnings" element={<Earnings/>} />
+      </Routes>
+    </BrowserRouter>
   </ChakraProvider>
 )
