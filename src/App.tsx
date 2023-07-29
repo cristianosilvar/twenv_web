45e64485c883f346815a7a00e728a859c3b5724c
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-28 21:54:57 -0300

    Adicionando Stack com o mês em 'Home'

--- src/App.tsx
+++ src/App.tsx
@@ -12,14 +12,19 @@ import Spending from "./pages/Spending/Spending"
 import Earnings from "./pages/Earnings/Earnings"
 
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from './pages/Home/Home'
 
 
 export const App = () => (
   <ChakraProvider theme={theme}>
     <Header/>
-    <Box width='80%' mx='auto' minH='60vh' border='1px solid #35383D' borderRadius='.75rem' bgColor='#0B0F18'>
+    <Box width='80%' mx='auto' minH='60vh' 
+      px='2rem' py='1rem'
+      border='1px solid #35383D' borderRadius='.75rem' 
+      bgColor='#0B0F18'>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home/>} />
           <Route path="/spending" element={<Spending/>} />
           <Route path="/earnings" element={<Earnings/>} />
         </Routes>
