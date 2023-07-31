1ea0a45a3fb3312f383d86d37abcbe8532990c9e
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-07-31 16:10:13 -0300

    Adicionando Links em Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,32 +1,21 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Link } from 'react-router-dom';
 import logo_twenv from '../../assets/logo-twenv.svg'
 
-import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Image, Link } from "@chakra-ui/react";
+import { Box, Breadcrumb, BreadcrumbItem, BreadcrumbLink, Image, Text } from "@chakra-ui/react";
 
 export default function Header() {
     return (
         <header>
             <Box w='80%' mx='auto' py='2.5rem' display='flex' alignItems='center' justifyContent='space-between'>
                 <Box display='flex' alignItems='center' gap='2rem'>
-                    <Image src={logo_twenv} width='100px'/>
+                    <Link to='/'>
+                        <Image src={logo_twenv} width='100px'/>
+                    </Link>
                     <Box w='.01rem' height='20px' bgColor='white' opacity='.5'/>
                     <Breadcrumb>
-                        <BreadcrumbItem fontSize='lg' fontWeight='light' gap='1.5rem' color='white'>
-                            {/* <BreadcrumbLink  opacity='.7' textDecoration='none'
-                            _hover={{
-                                opacity: 1
-                            }}>
-                                Despesas
-                            </BreadcrumbLink>
-                            <BreadcrumbLink  opacity='.7' textDecoration='none'
-                            _hover={{
-                                opacity: 1
-                            }}>
-                                Ganhos
-                            </BreadcrumbLink> */}
-
+                        <BreadcrumbItem fontSize='lg' fontWeight='normal' gap='1.5rem' color='white'>
                             <NavLink to='spending'>
-                                <BreadcrumbLink  opacity='.7' textDecoration='none'
+                                <BreadcrumbLink  opacity='.8' textDecoration='none'
                                 _hover={{
                                     opacity: 1
                                 }}>
@@ -34,7 +23,7 @@ export default function Header() {
                                 </BreadcrumbLink>
                             </NavLink>
                             <NavLink to='earnings'>
-                                <BreadcrumbLink  opacity='.7' textDecoration='none'
+                                <BreadcrumbLink  opacity='.8' textDecoration='none'
                                 _hover={{
                                     opacity: 1
                                 }}>
@@ -44,10 +33,14 @@ export default function Header() {
                         </BreadcrumbItem>
                     </Breadcrumb>
                 </Box>
-                <Link fontSize='xl' _hover={{
+                <Text fontSize='xl' _hover={{
                      textDecoration: 'none',
                      opacity: .7
-                }}>Entrar</Link>
+                }}>
+                    <Link to=''>
+                        Entrar
+                    </Link>
+                </Text>
             </Box>
         </header>
     )
