11401af481c6da1021e693db6856e4f0a886b494
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-08-01 17:32:10 -0300

    Adicionando Card: earnings e newEarning em Earnings

--- src/pages/Earnings/Earnings.tsx
+++ src/pages/Earnings/Earnings.tsx
@@ -1,6 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+
+import Card from "../../components/Card/Card";
+
 export default function Earnings() {
     return(
-        <>
-        </>
+        <section>
+            <Flex gap='4' wrap='wrap'>
+                <Card is="earnings" value={2000} date="01/07" description="Salário" />
+                
+                <Card is="newEarning"/>
+            </Flex>
+        </section>
     )
 }
