f0b1ba6baa2ee595cfadc896335ac61a17a85da4
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-11-12 18:35:28 -0300

    refactor: navigate in header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { Logo } from '../../icons/index'
 
 import { Box, Center } from '@chakra-ui/react'
 import Navbar from '../Navbar'
 
 export default function Header() {
+  const navigate = useNavigate()
+
   return (
     <>
       <Box w="80%" mx="auto">
         <Center>
           <Link to="/">
-            <Logo boxSize={'120px'} />
+            <Logo boxSize={'120px'} onClick={() => navigate('/')} />
           </Link>
         </Center>
         <Navbar />
