a4327e00cade768a05f4368933b213335cc7592b
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-11-12 21:53:12 -0300

    feat: add dashboard heading

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,3 +1,12 @@
+import { Box, Heading, Text } from '@chakra-ui/react'
+
 export default function Home() {
-  return <></>
+  return (
+    <Box w="80%" marginInline="auto" mt="30px" gap="30px">
+      <Heading as="h2">Dashboard</Heading>
+      <Text fontWeight="600" color="#fefefe50">
+        26 de Junho, 2023
+      </Text>
+    </Box>
+  )
 }
