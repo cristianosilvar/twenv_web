e033bbd26bfcd1269420d1b0faf3b60080cceb7c
“cristianosilvar” <cristianosilva2k@hotmail.com>
2023-12-10 19:21:57 -0300

    feat: deleted register page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { AuthProvider } from 'context/authContext'
+
 import theme from './theme'
+import { AuthProvider } from 'context/authContext'
 
 import Header from './components/Header'
 
-import Home from './pages/Home/Home'
-import RegisterUser from './pages/Register'
-import Spending from './pages/Spending/Spending'
-import Earnings from './pages/Earnings/Earnings'
+import Home from './pages/Home'
+import Spending from './pages/Spending'
+import Earnings from './pages/Earnings'
 
 export const App = () => (
   <AuthProvider>
@@ -20,7 +20,6 @@ export const App = () => (
           <Route path="/" element={<Home />} />
           <Route path="/spendings" element={<Spending />} />
           <Route path="/earnings" element={<Earnings />} />
-          <Route path="/user-register" element={<RegisterUser />} />
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
