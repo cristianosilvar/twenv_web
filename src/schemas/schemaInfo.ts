e5a10651bb640eec6db8a5c1f92f5be691625ba1
cristianosilvar <cristianosilva2k@hotmail.com>
2024-02-08 12:21:50 -0300

    fix: message error displayed in form validation

--- src/schemas/schemaInfo.ts
+++ src/schemas/schemaInfo.ts
@@ -3,7 +3,9 @@ import { z } from 'zod'
 export const schemaSpending = z.object({
   description: z.string(),
   date: z.string(),
-  value: z.coerce.number().min(0.01, 'O valor deve ser igual ou superior a 1'),
+  value: z.coerce
+    .number()
+    .min(0.01, 'O valor deve ser igual ou superior a R$ 0,01'),
 })
 
 export type spendingT = z.infer<typeof schemaSpending>
